a7c812fb47a586377dc4811c185294d3850d0d36
Mattia <mattia.bargellini@gmail.com>
2025-12-13 19:10:37 +0100

    Add atlantafx Version 0.6

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,10 @@
 module NexusFX {
+    requires atlantafx.base;
+
     requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.desktop;
 
     exports com.maemlab.nexusfx.dirty;
 }
