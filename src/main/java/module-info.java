fc8717ea4aeea2e5131502e24d6defb3ed457cd7
Mattia <mattia.bargellini@gmail.com>
2025-12-14 19:12:05 +0100

    Exported missing packages and subpackages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,5 +6,17 @@ module NexusFX {
     requires javafx.controls;
     requires java.desktop;
 
+    exports com.maemlab.nexusfx;
     exports com.maemlab.nexusfx.dirty;
+    exports com.maemlab.nexusfx.dialogs.alert;
+    exports com.maemlab.nexusfx.dialogs.modal;
+    exports com.maemlab.nexusfx.filters;
+    exports com.maemlab.nexusfx.formatters;
+    exports com.maemlab.nexusfx.listview.cells;
+    exports com.maemlab.nexusfx.listview.enhancedchecklistview;
+    exports com.maemlab.nexusfx.nav;
+    exports com.maemlab.nexusfx.panes.browse;
+    exports com.maemlab.nexusfx.panes.twocolumns;
+    exports com.maemlab.nexusfx.panes.twocolumns.row;
+    exports com.maemlab.nexusfx.toolbar;
 }
